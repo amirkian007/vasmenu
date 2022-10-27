1fd014f3caa64789da9fbda22336209b407d2d3e
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-10-28 00:44:28 +0330

    new build

--- src/main.js
+++ src/main.js
@@ -26,13 +26,9 @@ const routes = [
   { path: '/styles', component: Styles },
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   strict: true,
-  history: createWebHistory(''),
+  history: createWebHistory('/vasmenu/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 ,behavior: 'smooth',}
