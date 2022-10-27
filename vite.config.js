b32d445f6a8bec5c2a16ca1acafd464ec3740d8d
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-10-28 00:30:13 +0330

    new build

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build : {outDir:"docs"},
   plugins: [
 		vue(),
 		vuetify({defaultAssets:false}),
