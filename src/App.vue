0bbe3d52bd3b9469ea099cb8899cbc1b1e8810b1
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-11-02 14:10:32 +0330

    Refactore some css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-app :class="{ bgDark: people }">
+  <v-app :class="{ bgDark: darkMode ,'white-theme':!darkMode }">
     <v-app-bar
       :elevation="3"
-      :theme="people ? 'dark' : 'white'"
+      :theme="darkMode ? 'dark' : 'white'"
       :style="{ paddingLeft: miniwidth, transition: 'all 0.3s' }"
     >
       <v-btn
@@ -22,7 +22,7 @@
       <v-btn-toggle rounded="0">
         <v-switch
           inset
-          v-model="people"
+          v-model="darkMode"
           color="primary"
           class="ml-1"
           style="padding-inline-start: 0px"
@@ -34,7 +34,7 @@
         </v-switch>
         <v-switch
           inset
-          v-model="people1"
+          v-model="fullMode"
           color="primary"
           class="ml-1"
           label="Full"
@@ -53,7 +53,7 @@
         >
           <path
             fill-rule="evenodd"
-            :fill="people?'white':''"
+            :fill="darkMode?'white':''"
             d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
           ></path>
         </svg>
@@ -68,8 +68,8 @@
       v-model:collapsed="collapsed"
       :menu="testmENU"
       vueRouterEnabel
-      :dark="people"
-      :menuType="people1 ? 'fully' : 'simple'"
+      :dark="darkMode"
+      :menuType="fullMode ? 'fully' : 'simple'"
       :autoCollapse="600"
       :closeOnClickOutSide="ismobile"
       :overLayerOnOpen="ismobile"
@@ -114,16 +114,6 @@ export default {
         }, 0);
       });
     },
-    people(val) {
-      if (val) {
-        const el = document.getElementById("whiteLink");
-        this.chace = el;
-        el.remove();
-      } else {
-        const el = document.getElementById("darkLink");
-        el.appendChild(this.chace);
-      }
-    },
   },
   methods: {
     itemClickHandel(item) {
@@ -154,8 +144,8 @@ export default {
   data: () => ({
     min: false,
     collapsed: false,
-    people: false,
-    people1: false,
+    darkMode: false,
+    fullMode: false,
     chace: null,
     screenWidth: 0,
     testmENU: [
